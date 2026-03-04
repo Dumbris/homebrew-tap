39df83db2f2ae28794baaa7a4dd776c2f00a5f4c
Dumbris <a.dumbris@gmail.com>
2026-03-04 15:13:34 +0200

    mc-zig 0.2.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/mc-zig.rb
+++ Formula/mc-zig.rb
@@ -1,13 +1,13 @@
 class McZig < Formula
   desc "Fast Midnight Commander clone built in Zig"
   homepage "https://github.com/Dumbris/mc-zig"
-  version "0.1.0"
+  version "0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Dumbris/mc-zig/releases/download/v#{version}/mc-aarch64-macos.tar.gz"
-      sha256 "PLACEHOLDER"
+      url "https://github.com/Dumbris/mc-zig/releases/download/v0.2.0/mc-aarch64-macos.tar.gz"
+      sha256 "56ee0e7077f652d77087f97a034e3b343852c94ae6530d27a8f349b1b4628b59"
     else
       url "https://github.com/Dumbris/mc-zig/releases/download/v#{version}/mc-x86_64-macos.tar.gz"
       sha256 "PLACEHOLDER"
